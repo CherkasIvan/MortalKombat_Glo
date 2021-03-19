99bb5cb236ae239d7db5f1d92678c1b634e27aa0
Cherkas Ivan <63066511+CherkasIvan@users.noreply.github.com>
2021-03-19 07:51:47 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    $('#app').vide({
+        mp4: 'video/video.mp4',
+        webm: 'video/video.webm',
+        ogv: 'video/video.ogv',
+        poster: 'img/cover-image/jpg'
+    });
+});
